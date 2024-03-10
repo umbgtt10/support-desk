9adb5db85571e89b5069f070118a561d2cc60b5d
umbertogotti <umbertogotti1979@gmail.com>
2024-03-10 18:11:23 +0100

    Express server configured

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send({message: 'Welcome to the support desk API'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`)
+})

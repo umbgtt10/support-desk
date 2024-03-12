a893be2713d3f874577dc6d1520e2ddc5ad68f34
umbertogotti <umbertogotti1979@gmail.com>
2024-03-12 05:34:58 +0100

    Error handler integrated

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded( {extended:false} ))
 
 app.get('/', (req, res) => {
     res.status(200).send({message: 'Welcome to the support desk API'})
 })
 
 app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`)

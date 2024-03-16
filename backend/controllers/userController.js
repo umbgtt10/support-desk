ea4cdf190a722f7df4b757fb50f74249ae008df4
umbertogotti <umbertogotti1979@gmail.com>
2024-03-16 07:01:47 +0100

    User Registered

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,44 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
 
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
+
+    console.log(req.body)
 
     if (!name || !email || !password){
        res.status(400)
        throw new Error('Please include all fields')
     }
 
-    console.log(req.body)
-    res.send('Register Route')
+    const userExists = await User.findOne({email:email})
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')        
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 const loginUser = asyncHandler(async (req, res) => {

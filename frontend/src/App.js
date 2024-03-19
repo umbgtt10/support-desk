5ec33b36e82b4b9abfabf18191ad33f01a777085
umbertogotti <umbertogotti1979@gmail.com>
2024-03-19 10:41:34 +0100

    Route Guard implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import NewTicket from './pages/NewTicket'
 
 function App() {
   return (
@@ -13,9 +15,12 @@ function App() {
         <div className='container'>
            <Header/>
            <Routes>
-              <Route path='/' element= {<Home/>}/>
-              <Route path='/login' element= {<Login/>}/>
-              <Route path='/register' element= {<Register/>}/>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+              <Route path='/new-ticket' element={<PrivateRoute/>}>
+                 <Route path='/new-ticket' element={<NewTicket/>}/>
+              </Route>
            </Routes>
         </div>
       </Router>
---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,9 @@
+function NewTicket() {
+  return (
+    <div>
+      <h1>NewTicket</h1>
+    </div>
+  )
+}
+
+export default NewTicket

26d6c5c4572cc518dec2e2c334b72bbff26ca2ec
umbertogotti <umbertogotti1979@gmail.com>
2024-03-21 10:53:29 +0100

    Ticket page implemented and wired

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import NewTicket from './pages/NewTicket'
 import Tickets from './pages/Tickets'
+import Ticket from './pages/Ticket'
 
 function App() {
   return (
@@ -34,7 +35,15 @@ function App() {
                       <Tickets />
                    </PrivateRoute>
                   }
-              />              
+              /> 
+              <Route
+                 path='/ticket/:ticketId'
+                 element={
+                   <PrivateRoute>
+                      <Ticket />
+                   </PrivateRoute>
+                  }
+              />                            
            </Routes>
         </div>
       </Router>
--- frontend/src/hooks/useAuthStatus.js
+++ frontend/src/hooks/useAuthStatus.js
@@ -6,7 +6,7 @@ export const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false)
     const [checkingStatus, setCheckingStatus] = useState(true)
 
-    const { user } = useSelector((state) => state.auct)
+    const { user } = useSelector((state) => state.auth)
 
     useEffect(() => {
        if (user) {

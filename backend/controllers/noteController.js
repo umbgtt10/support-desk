1161fadee294d8fb046f4a70ae14598dc3b02869
umbertogotti <umbertogotti1979@gmail.com>
2024-03-22 16:59:19 +0100

    Backend Note support added

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,57 @@
+const asyncHandler = require('express-async-handler')
+
+const User = require('../models/userModel')
+const Note = require('../models/noteModel')
+const Ticket = require('../models/ticketModel')
+
+const getNotes = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user.id)
+
+    if (!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.ticketId)
+
+    if (ticket.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    const notes = await Note.find({ticket:req.params.ticketId})
+
+    res.status(200).json(notes)
+})
+
+const addNote = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user.id)
+
+    if (!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.ticketId)
+
+    if (ticket.user.toString() !== req.user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+    const note = await Note.create({
+        ticket:req.params.ticketId,
+        text: req.body.text,
+        isStaff: false,
+        user: req.user.id
+    })
+
+    res.status(201).json(note)
+})
+
+module.exports = {
+    getNotes,
+    addNote
+}

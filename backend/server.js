a0a604b0fe5b93b4a75072bf0e9c95082b1ed4d5
umbertogotti <umbertogotti1979@gmail.com>
2024-03-24 07:36:09 +0100

    Deployment issue fixed III

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded( {extended:false} ))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-if (process-env-NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))

1f823dcc6252574b50bf82e2235de0b413050d2f
umbertogotti <umbertogotti1979@gmail.com>
2024-03-24 08:03:49 +0100

    Deployment issue fixed IV

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,34 @@
 const path = require('path')
 const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler} = require('./middleware/errorMiddleware')
+require('colors')
+require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+// Connect to database
 connectDB()
 
 const app = express()
+
 app.use(express.json())
-app.use(express.urlencoded( {extended:false} ))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
-    app.get('/', (req, res) => {
-       res.status(200).send({message: 'Welcome to the support desk API'})
-    })
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
 }
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
